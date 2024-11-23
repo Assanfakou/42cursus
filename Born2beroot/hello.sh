8215211b58254773384d998be24ccd5a084b3d9d
Assankou <fakouhasan@gmail.com>
2024-11-23 18:12:58 +0100

    the baby is comming

--- Born2beroot/hello.sh
+++ Born2beroot/hello.sh
@@ -11,6 +11,18 @@ vpro=$(grep "processor" /proc/cpuinfo | wc -l)
 
 echo "vCPU : $vpro"
 
-mem=$(free -m | grep "Mem" | awk '{print $3}')
+memused=$(free -m | grep "Mem" | awk '{print $3}')
 
-echo "Memory Usage: $mem"
+memorytotal=$(free -m | grep "Mem" | awk '{print $2}')
+
+percentage=$(free -m | grep "Mem" | awk '{printf"%.2f", ($3/$2)*100}')
+
+echo "Memory Usage: $memused/$memorytotal"MB" ($percentage%)"
+
+Diskuse=$(df -m | grep "/dev/" | grep -v  "/boot" | awk '{memoryuse += $3} END {print memoryuse}')
+
+Disktotal=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{memorytotal += $2} END {printf ("%.fGB"), memorytotal/1024}')
+percent=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{per += $2} {prs += $3} END {printf("%d%%"), prs/per * 100}')
+echo "Disk Usage: $Diskuse/$Disktotal ($percent)"
+
+
