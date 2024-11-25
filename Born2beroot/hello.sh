f2349e5c021c3fbe12e09fb3c9aad09fe69e87df
Assankou <fakouhasan@gmail.com>
2024-11-25 14:04:25 +0100

    the baby still comming

--- Born2beroot/hello.sh
+++ Born2beroot/hello.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 arch=$(uname -a)
-echo "Architecture: $arch"
+echo "#Architecture: $arch"
 
 pro=$(grep "physical id" /proc/cpuinfo | wc -l)
 
-echo "CPU physical : $pro"
+echo "#CPU physical : $pro"
 
 vpro=$(grep "processor" /proc/cpuinfo | wc -l)
 
-echo "vCPU : $vpro"
+echo "#vCPU : $vpro"
 
 memused=$(free -m | grep "Mem" | awk '{print $3}')
 
@@ -17,12 +17,20 @@ memorytotal=$(free -m | grep "Mem" | awk '{print $2}')
 
 percentage=$(free -m | grep "Mem" | awk '{printf"%.2f", ($3/$2)*100}')
 
-echo "Memory Usage: $memused/$memorytotal"MB" ($percentage%)"
+echo "#Memory Usage: $memused/$memorytotal"MB" ($percentage%)"
 
 Diskuse=$(df -m | grep "/dev/" | grep -v  "/boot" | awk '{memoryuse += $3} END {print memoryuse}')
 
 Disktotal=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{memorytotal += $2} END {printf ("%.fGB"), memorytotal/1024}')
 percent=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{per += $2} {prs += $3} END {printf("%d%%"), prs/per * 100}')
-echo "Disk Usage: $Diskuse/$Disktotal ($percent)"
-
-
+echo "#Disk Usage: $Diskuse/$Disktotal ($percent)"
+cpuload=$(vmstat 1 2 | tail -1 | awk '{print $15}')
+echo "#CPU louad: $cpuload%"
+reboot=$(uptime -s | awk -F: '{print $1 ":" $2}')
+echo "#Last boot: $reboot"
+lvmuse=$(lsblk | grep -q lvm && echo yes || echo no)
+echo "#LVM : $lvmuse"
+tcp=$(ss -ta | grep ESTAB | wc -l)
+echo "#TCP : $tcp ESTABLISHED"
+user=$(users | wc -w)
+echo "#User log : $user"
