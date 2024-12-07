b1a0506f52e5fc4f73f8dbe31290493f605c84ad
Assan kou <138638488+Assanfakou@users.noreply.github.com>
2024-12-07 14:29:45 +0000

    norm2

--- ft_printf/ft_checker.c
+++ ft_printf/ft_checker.c
@@ -1,24 +1,24 @@
 #include "ft_printf.h"
 
-int ft_checker(va_list arg, char s)
+int	ft_checker(va_list arg, char s)
 {
-    int i;
+	int i;
 
-    if (s == 'd' || s == 'i')
-        i = ft_putnbr(va_arg(arg, int));
-    else if (s == 'x')
-        i = ft_putnum_lhex(va_arg(arg, unsigned int));
-    else if (s == 'X')
-        i = ft_putnum_uhex(va_arg(arg, unsigned int));
-    else if (s == 's')
-        i = ft_putstr(va_arg(arg, char *));
-    else if (s == 'c')
-        i = ft_putchar(va_arg(arg, int));
-    else if (s == 'u')
-        i = ft_uuputnb(va_arg(arg, unsigned int));
-    else if (s == 'p')
-        i = ft_putaddre(va_arg(arg, unsigned long));
-    else if (s == '%')
-        i = ft_putchar('%');
-    return (i);
+	if (s == 'd' || s == 'i')
+		i = ft_putnbr(va_arg(arg, int));
+	else if (s == 'x')
+		i = ft_putnum_lhex(va_arg(arg, unsigned int));
+	else if (s == 'X')
+		i = ft_putnum_uhex(va_arg(arg, unsigned int));
+	else if (s == 's')
+		i = ft_putstr(va_arg(arg, char *));
+	else if (s == 'c')
+		i = ft_putchar(va_arg(arg, int));
+	else if (s == 'u')
+		i = ft_uuputnb(va_arg(arg, unsigned int));
+	else if (s == 'p')
+		i = ft_putaddre(va_arg(arg, unsigned long));
+	else if (s == '%')
+		i = ft_putchar('%');
+	return (i);
 }
