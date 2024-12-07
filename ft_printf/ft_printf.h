de2e5f19d5d886b7a1b34149fafc3239c2fc63fc
Assan kou <138638488+Assanfakou@users.noreply.github.com>
2024-12-07 14:45:43 +0000

    header

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,14 +1,26 @@
-#include <unistd.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hfakou <hfakou@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 14:44:42 by hfakou            #+#    #+#             */
+/*   Updated: 2024/12/07 14:44:45 by hfakou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int     ft_putnbr(int n);
-int     ft_putstr(char *st);
-int     ft_putchar(char c);
-int     ft_putnum_uhex(unsigned int nbr);
-int     ft_putnum_lhex(unsigned int nb);
-int     ft_uuputnb(unsigned int nbs);
-int     ft_putaddre(unsigned long add);
-int ft_checker(va_list arg, char s);
-int ft_printf(const char *firstpar, ... );
+int	ft_putnbr(int n);
+int	ft_putstr(char *st);
+int	ft_putchar(char c);
+int	ft_putnum_uhex(unsigned int nbr);
+int	ft_putnum_lhex(unsigned int nb);
+int	ft_uuputnb(unsigned int nbs);
+int	ft_putaddre(unsigned long add);
+int	ft_checker(va_list arg, char s);
+int	ft_printf(const char *firstpar, ...);
