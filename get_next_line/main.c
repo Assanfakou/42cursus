f8eef46426b01e1fa352a6fcf667d60c7da1040a
Assankou <fakouhasan@gmail.com>
2024-12-21 22:09:14 +0100

    Get_Next_line 10

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -8,15 +8,38 @@ int main ()
 	line = get_next_line(fd);
 	printf("%s", line);
 	free(line);
-	close(fd);
+
 	char *line2;
 	line2 = get_next_line(fd);
 	printf("%s", line2);
 	free(line2);
-	close(fd);
+
 	char *line3;
 	line3 = get_next_line(fd);
 	printf("%s", line3);
 	free(line3);
+
+	char *line4;
+	line4= get_next_line(fd);
+	printf("%s", line4);
+
+	char *line5;
+	line5= get_next_line(fd);
+	printf("%s", line5);
+	free(line5);
+
+	char *line6;
+	line6 =get_next_line(fd);
+	printf("%s", line6);
+	free(line6);
+
+	char *line7;
+	line7=get_next_line(fd);
+	printf("%s", line7);
+	free(line7);
+	char *line8;
+	line8=get_next_line(fd);
+	printf("%s", line8);
+	free(line8);
 	close(fd);
 }
