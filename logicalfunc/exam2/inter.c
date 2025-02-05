3ab464494608ccdd8bd36822923de37f2f438771
Assankou <fakouhasan@gmail.com>
2025-02-05 22:52:55 +0100

    makefie

--- logicalfunc/exam2/inter.c
+++ logicalfunc/exam2/inter.c
@@ -27,21 +27,21 @@ int main (int ac, char **av)
         i = 0;
         while (av[1][i])
         {
-            if (check(av[1], av[1][i], i) == 0)
+            if (!check(av[1], av[1][i], i) && check(av[2], av[1][i], strlen(av[2])))
             {
                 write(1, &av[1][i], 1);
             }
             i++;
         }
         j = 0;
-        while (av[2][j])
-        {
-            if (check(av[1], av[2][j], i) == 0 && check(av[2], av[2][j], j) == 0)
-            {
-                write(1, &av[2][j], 1);
-            }
-            j++;
-        }
-    }
+    //     while (av[2][j])
+    //     {
+    //         if (!check(av[1], av[2][j], i) && !check(av[2], av[2][j], j))
+    //         {
+    //             write(1, &av[2][j], 1);
+    //         }
+    //         j++;
+    //     }
+    // }
     write(1, "\n", 1);
 }
--- push_swap/main.c
+++ push_swap/main.c
@@ -3,57 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: assankou <assankou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hfakou <hfakou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 10:01:09 by hfakou            #+#    #+#             */
-/*   Updated: 2025/02/05 11:30:16 by assankou         ###   ########.fr       */
+/*   Updated: 2025/02/05 21:28:53 by hfakou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push.h"
-// int *fill_arr(s_stack *stack_h)
-// {
-//     int *arr;
-//     int size_lis;
-//     int i;
+int *fill_arr(s_stack *stack_h)
+{
+    int *arr;
+    int size_lis;
+    int i;
 
-//     size_lis = listint_len(stack_h);
+    size_lis = listint_len(stack_h);
     
-//     arr = malloc(sizeof(int) * (size_lis));
-//     if (!arr)
-//         return (NULL);
-//     i = 0;
-//     while (i < size_lis && stack_h)
-//     {
-//         arr[i] = stack_h->num;
-//         i++;
-//         stack_h = stack_h->next;
-//     }
-//     return (arr);
-// }
-void *sort_arr(int *arr)
+    arr = malloc(sizeof(int) * (size_lis));
+    if (!arr)
+        return (NULL);
+    i = 0;
+    while (i < size_lis && stack_h)
+    {
+        arr[i] = stack_h->num;
+        i++;
+        stack_h = stack_h->next;
+    }
+    return (arr);
+}
+void sort_arr(int *arr, int size)
 {
     int i;
     int j;
-    int sorted;
-    int temp;
-    int size;
 
-    size = 0;
-    while (arr[size])
-        size++;
     i = 0;
-    while (i < (size - 1))
+    while (i < size)
     {
-        sorted = 0;
-        j = 0;
-        while (j < (size - i - 1))
+        int sorted = 0;
+        j = i + 1;
+        while (j < size)
         {
-            if (arr[j] > arr[j + 1])
+            if (arr[i] > arr[j])
             {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
                 sorted = 1;
             }
             j++;
@@ -63,40 +57,29 @@ void *sort_arr(int *arr)
             break;
     }
 }
-// int main(int ac, char **av)
-// {
-//     int i;
-//     int *arr;
-//     s_stack *stack_a = NULL;
-//     s_stack *stack_b = NULL;
-    
-//    int j = ac - 1;
-
-//     stack_a = pars_args(&av[1], j);
-//     if (!stack_a)
-//         write(2, "Erour\n", 6);
-//     // print_listint(stack_a);
-//     arr = fill_arr(stack_a);
-//     // for(int i = 0; i < 14; i++)
-//     //     ft_printf("%d\n", arr[i]);
-//     sort_arr(arr);
-//    i = 0;
-//    while (i < 9){
-//         ft_printf("%d\n", arr[i]);
-//         i++;
-//     }
-
-// }
-int main ()
+int main(int ac, char **av)
 {
-    int a[] = {18, 2, 6};
+    int i;
+    int *arr;
+    s_stack *stack_a = NULL;
+    s_stack *stack_b = NULL;
+    
+   int j = ac - 1;
 
-    sort_arr(a);
-    int i = 0;
-    // while (i < 5)
-    // {  
-        printf("%d\n efr", 4);
-        // i++;
-    // }
+    stack_a = pars_args(&av[1], j);
+    if (!stack_a)
+        write(2, "Erour\n", 6);
+    // print_listint(stack_a);
+    arr = fill_arr(stack_a);
+    // for(int i = 0; i < 14; i++)
+    //     ft_printf("%d\n", arr[i]);
+    sort_arr(arr, listint_len(stack_a));
+   i = 0;
+   while (i < 9){
+        ft_printf("%d\n", arr[i]);
+        i++;
+    }
+
 }
+
 
