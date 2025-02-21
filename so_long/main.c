d54cb990d2acf8cfac6991c9d4e7c50cdf7977aa
Assankou <fakouhasan@gmail.com>
2025-02-21 20:50:38 +0100

    adding the door 1

--- so_long/main.c
+++ so_long/main.c
@@ -6,14 +6,12 @@
 /*   By: hfakou <hfakou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 09:25:52 by hfakou            #+#    #+#             */
-/*   Updated: 2025/02/21 12:57:09 by hfakou           ###   ########.fr       */
+/*   Updated: 2025/02/21 20:50:11 by hfakou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "so_long.h"
-
-void draw_map(t_game *game);
 
 void player_pos(t_game *game)
 {
@@ -36,11 +34,12 @@ void player_pos(t_game *game)
         y++;
     }
 }
+
 int mv_player(int key_code, t_game *game)
 {
     int pos_x = game->pos_play_x;
     int pos_y = game->pos_play_y;
-   
+
     if (key_code == UP || key_code == 'w')
         pos_y--;
     if (key_code == DOWN || key_code == 's')
@@ -61,9 +60,12 @@ int mv_player(int key_code, t_game *game)
         }
     return (0);
 }
-int handle_keypress(int keycode, void *game)
+int handle_keypress(int keycode, t_game *game)
 {
+       int coin = 0;
     mv_player(keycode, game);
+    // if (game->ma_p[game->pos_play_y][game->pos_play_x] == 'C')
+    //                     coin++; // doesn't work
     if (keycode == UP || keycode == 'w')
         printf("tap UP\n");
     else if (keycode == RIGHT || keycode == 'd')
@@ -74,11 +76,12 @@ int handle_keypress(int keycode, void *game)
         printf("tap DOWN\n");
     else if (keycode == ESC || keycode == 113)
     {
+        printf("coins %d\n", coin);
         if (keycode == 113)
             printf("tap Q\n");
         else
             printf("tap ESC\n");
-        mlx_destroy_window(((t_game *)game)->mlx, ((t_game *)game)->win);
+        mlx_destroy_window(game->mlx, game->win);
         exit(EXIT_SUCCESS);
     }
     return (0);
@@ -90,20 +93,20 @@ void fill_map_struct(t_game *game)
     char map[HEIGHT][WIDTH] = {
         {'1', '1', '1', '1', '1', '1', '1', '1', '1'},
         {'1', 'E', '1', '1', '1', '1', '1', 'P', '1'},
-        {'1', 'C', '1', '1', '1', '0', '0', '0', '1'},
-        {'1', '0', '1', '1', '0', 'C', '1', '0', '1'},
+        {'1', '0', '1', '1', '1', '0', '0', '0', '1'},
+        {'1', 'C', '1', '1', '0', 'C', '1', '0', '1'},
         {'1', '0', '1', '0', '0', '1', '1', '0', '1'},
         {'1', '0', '0', '0', '1', '1', '1', '0', '1'},
         {'1', '1', 'C', '1', '1', '1', '1', '0', '1'},
-        {'1', '0', '0', '0', '0', '0', '0', '0', '1'},
+        {'1', '0', '0', '0', '0', '0', '0', '1', '1'},
         {'1', '1', '1', '1', '1', '1', '1', '1', '1'}
     };
 
     y = 0;
-    while (y < 9)
+    while (y < WIDTH)
     {
         x = 0;
-        while (x < 9)
+        while (x < HEIGHT)
         {
             game->ma_p[y][x] = map[y][x];
             x++;
@@ -118,17 +121,23 @@ void draw_map(t_game *game)
 
     player_pos(game);
     y = 0;
-    while (y < 9)
+    while (y < WIDTH)
     {
         x = 0;
-        while (x < 9)
+        while (x < HEIGHT)
         {
             if (game->ma_p[y][x] == '1')
                 mlx_put_image_to_window(game->mlx, game->win, game->wall_img, x * TILE_SIZE, y * TILE_SIZE);
             else if (game->ma_p[y][x] == 'P')
                 mlx_put_image_to_window(game->mlx, game->win, game->player_img, x * TILE_SIZE, y * TILE_SIZE);
             else if (game->ma_p[y][x] == 'C')
-                mlx_put_image_to_window(game->mlx, game->win, game->coin_img, x * TILE_SIZE, y * TILE_SIZE);
+            {
+                    // mlx_put_image_to_window(game->mlx, game->win, game->coin_img_1, x * TILE_SIZE, y * TILE_SIZE);
+                // while ('P' != 'C')
+                // {
+                    mlx_put_image_to_window(game->mlx, game->win, game->coin_img_2, x * TILE_SIZE, y * TILE_SIZE);
+                // }
+            }
             else if (game->ma_p[y][x] == 'E')
                 mlx_put_image_to_window(game->mlx, game->win, game->exit_img, x * TILE_SIZE, y * TILE_SIZE);
             x++;
@@ -142,19 +151,20 @@ int main(void)
     int y;
     int x;
 
-    // initializing the stuct's map
+    //fill map func to initializ the map
     fill_map_struct(&game);
 
     game.mlx = mlx_init();
     game.win = mlx_new_window(game.mlx, WIDTH * TILE_SIZE, HEIGHT * TILE_SIZE, "so_long");
 
     game.wall_img = mlx_xpm_file_to_image(game.mlx, "pngs/123.xpm", &(int){TILE_SIZE}, &(int){TILE_SIZE});
-    game.player_img = mlx_xpm_file_to_image(game.mlx, "pngs/p1.xpm", &(int){TILE_SIZE}, &(int){TILE_SIZE});
-    game.coin_img = mlx_xpm_file_to_image(game.mlx, "coin_pngs/AnyConv.com__coin2.xpm", &(int){TILE_SIZE}, &(int){TILE_SIZE});
+    game.player_img = mlx_xpm_file_to_image(game.mlx, "pngs/Run.xpm", &(int){TILE_SIZE}, &(int){TILE_SIZE});
+    game.coin_img_1 = mlx_xpm_file_to_image(game.mlx, "coin_pngs/AnyConv.com__coin2.xpm", &(int){TILE_SIZE}, &(int){TILE_SIZE});
+    game.coin_img_2 = mlx_xpm_file_to_image(game.mlx, "coin_pngs/AnyConv.com__coin6.xpm", &(int){TILE_SIZE}, &(int){TILE_SIZE});
     game.exit_img = mlx_xpm_file_to_image(game.mlx, "pngs/Door.xpm", &(int){TILE_SIZE}, &(int){TILE_SIZE});
 
     draw_map(&game);
-    printf("x = %d, y = %d\n", game.pos_play_x, game.pos_play_y);
+    // printf("x = %d, y = %d\n", game.pos_play_x, game.pos_play_y);
     mlx_key_hook(game.win, handle_keypress, &game);
     mlx_loop(game.mlx);
     return (0);
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -19,12 +19,14 @@
 # include <mlx.h>
 
 # define WINDOW 369
-# define TILE_SIZE 41
+# define TILE_SIZE 45
+
 # define LEFT 65361
 # define UP 65362 
 # define RIGHT 65363
 # define DOWN 65364
 # define ESC 65307
+
 # define WIDTH 9
 # define HEIGHT 9
 
@@ -43,10 +45,14 @@ typedef struct s_game
 
     void *wall_img;
     void *player_img;
-    void *coin_img;
+    void *coin_img_1;
+    void *coin_img_2;
     void *exit_img;
 }       t_game;
 
 void draw_map(t_game *game);
+int mv_player(int key_code, t_game *game);
+int handle_keypress(int keycode, t_game *game);
+void fill_map_struct(t_game *game);
 
 # endif
